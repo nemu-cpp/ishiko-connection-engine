972cc02123a8a6264d27ad5c25ae554096ddb3f9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-14 22:21:14 +0100

    Updates

---
+++ tests/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2019-2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/ishiko-connection-engine/blob/main/LICENSE.txt
+*/
+
+#include <Ishiko/Tests.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("NemuIshikoConnectionEngine");
+
+    return theTestHarness.run();
+}
