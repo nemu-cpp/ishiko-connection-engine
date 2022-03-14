0cd6495cca90f6eaeecd73c3d30675c63bde5020
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-14 22:11:12 +0000

    Add files

---
+++ src/IshikoConnectionEngine.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/ishiko-connection-engine/blob/main/LICENSE.txt
+*/
+
+#include "IshikoConnectionEngine.hpp"
