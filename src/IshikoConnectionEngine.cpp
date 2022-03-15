fac7ae575a50d38e4725f412dd822eed4bd2bf70
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-15 14:17:03 +0000

    Delete file

--- src/IshikoConnectionEngine.cpp
+++
@@ -1,7 +0,0 @@
-/*
-    Copyright (c) 2022 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/nemu-cpp/ishiko-connection-engine/blob/main/LICENSE.txt
-*/
-
-#include "IshikoConnectionEngine.hpp"
