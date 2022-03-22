4ed5d5542d47b6b45c61e30fd19018857a2388b6
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-22 20:51:32 +0000

    Add files

---
+++ include/Nemu/IshikoConnectionEngine/IshikoWebResponseBuilder.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/ishiko-connection-engine/blob/main/LICENSE.txt
+*/
+
+#ifndef _NEMU_CPP_ISHIKOCONNECTIONENGINE_ISHIKOWEBRESPONSEBUILDER_HPP_
+#define _NEMU_CPP_ISHIKOCONNECTIONENGINE_ISHIKOWEBRESPONSEBUILDER_HPP_
+
+#endif
