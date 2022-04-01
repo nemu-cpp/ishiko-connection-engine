73e74c5e2f017a2bc55b6f68f2ed1e6b7cf9e033
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-01 15:44:34 +0100

    Updates

---
+++ include/Nemu/WebFramework/WebResponseBuilder.hpp
@@ -0,0 +1,48 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/web-framework/blob/main/LICENSE.txt
+*/
+
+#ifndef _NEMU_CPP_WEBFRAMEWORK_WEBRESPONSEBUILDER_HPP_
+#define _NEMU_CPP_WEBFRAMEWORK_WEBRESPONSEBUILDER_HPP_
+
+#include <Ishiko/HTTP.hpp>
+#include <string>
+
+namespace Nemu
+{
+
+/// An interface to create the response that will be sent back to the client.
+class WebResponseBuilder
+{
+public:
+    WebResponseBuilder();
+
+    void setStatus(unsigned int status);
+    void setDateHeader(const Ishiko::UTCTime& time);
+    std::string& body();
+
+    std::string toString() const;
+
+    /// Renders a page using the default templating engine.
+    /**
+        @param viewName The name of the view to render. How this name is used to find the right template depends on the
+        engine and its options.
+    */
+    void view(const std::string& viewName);
+
+    void redirect();
+
+    void state();
+
+private:
+    Ishiko::HTTPResponse m_response;
+    // TODO: I need to refactor HTTPResponse to make this more flexible and remove the redundant and wasteful m_body
+    std::string m_body;
+    const Views* m_views;
+};
+
+}
+
+#endif
