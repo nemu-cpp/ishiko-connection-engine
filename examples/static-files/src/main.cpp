e56b4b7b71a9cd1f5062a3cad614a28ec459f963
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-02 16:12:23 +0100

    Fix example code compilation

--- examples/static-files/src/main.cpp
+++ examples/static-files/src/main.cpp
@@ -4,15 +4,15 @@
     See https://github.com/nemu-cpp/nemu/blob/main/LICENSE.txt
 */
 
-#include <Nemu.hpp>
+#include <Nemu/WebFramework.hpp>
 
 int main(int argc, char* argv[])
 {
     Ishiko::Error error;
 
     // TODO: use the async server
-    std::shared_ptr<Nemu::IshikoSingleConnectionServer> server =
-        std::make_shared<Nemu::IshikoSingleConnectionServer>(Ishiko::TCPServerSocket::AllInterfaces,
+    std::shared_ptr<Nemu::SingleConnectionWebServer> server =
+        std::make_shared<Nemu::SingleConnectionWebServer>(Ishiko::TCPServerSocket::AllInterfaces,
             Ishiko::Port::http, error);
 
     // Create a log that sends its output to the console.
