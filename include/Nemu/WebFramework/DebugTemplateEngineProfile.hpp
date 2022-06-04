a1b9a67335b4ebf91b546a5b3a62d5dffa96ac6c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-04 17:15:28 +0100

    Updates

---
+++ include/Nemu/WebFramework/DebugTemplateEngineProfile.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/web-framework/blob/main/LICENSE.txt
+*/
+
+#ifndef _NEMU_CPP_WEBFRAMEWORK_DEBUGTEMPLATEENGINEPROFILE_HPP_
+#define _NEMU_CPP_WEBFRAMEWORK_DEBUGTEMPLATEENGINEPROFILE_HPP_
+
+#endif
